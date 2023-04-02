a24fae437669730fac7d4750b6dc79441c91d2bf
n01521239 <106199863+n01521239@users.noreply.github.com>
2023-04-02 15:11:35 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://9e4lv517tf.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "https://3zx9a2ni8k.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
